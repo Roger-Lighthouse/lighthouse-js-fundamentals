eefd2b6e9011faa5d067923a61ed5a7e564ec54c
Roger-Lighthouse <roger@whiteshark.ca>
2017-01-02 02:36:43 +0000

    add function odd excercise

---
+++ odd.js
@@ -0,0 +1,13 @@
+function print(item){
+	console.log(item);
+}
+
+function isOdd(num){
+	return num % 2===1;
+}
+
+var num_a=10;
+var num_b=11;
+
+print(num_a+" is odd is "+isOdd(num_a));
+print(num_b+" is odd is "+isOdd(num_b));
