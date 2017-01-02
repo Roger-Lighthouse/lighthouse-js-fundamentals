e7169f978db47a474b188e096c7388b85eb2c396
Roger-Lighthouse <roger@whiteshark.ca>
2017-01-02 02:30:14 +0000

    add function excercises

---
+++ even.js
@@ -0,0 +1,13 @@
+function print(item){
+	console.log(item);
+}
+
+function isEven(num){
+	return num % 2===0;
+}
+
+var num_a=10;
+var num_b=11;
+
+print(num_a+" is even is "+isEven(num_a));
+print(num_b+" is even is "+isEven(num_b));
